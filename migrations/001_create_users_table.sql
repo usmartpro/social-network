7144b31f7acb670230c390753c97e878e9f83a5a
artur.usmanov <artur.usmanov@x5.ru>
2025-04-01 00:12:21 +0300

    дефолтные значения с поля дат создания и обновления данных

--- migrations/001_create_users_table.sql
+++ migrations/001_create_users_table.sql
@@ -1,17 +1,17 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS users
 (
-    id          uuid default gen_random_uuid() not null
-    constraint users_pk
-    primary key,
-    first_name  varchar(100)                   not null,
-    second_name varchar(100)                   not null,
+    id          uuid      default gen_random_uuid() not null
+        constraint users_pk
+            primary key,
+    first_name  varchar(100)                        not null,
+    second_name varchar(100)                        not null,
     birthdate   date,
     city        varchar(100),
     biography   text,
-    password    varchar(60)                    not null,
-    created_at  timestamp                      not null,
-    updated_at  timestamp                      not null
+    password    varchar(60)                         not null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null,
+    updated_at  timestamp default CURRENT_TIMESTAMP not null
 );
 
 -- +goose Down
